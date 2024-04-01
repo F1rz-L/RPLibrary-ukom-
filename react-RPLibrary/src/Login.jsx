58f1c1f061c43180ed95dae46ef36bd3d5b8770d
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2024-04-01 11:15:24 +0700

    pattern

--- react-RPLibrary/src/Login.jsx
+++ react-RPLibrary/src/Login.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './pattern.css'
 
 function Login() {
     return (
         <>
-            <form className="container flex justify-center items-center flex-col w-3/4 gap-4 h-svh">
-                <h1 className='text-5xl font-bold mb-4 -mt-12'>Login</h1>
-                <label className="input input-bordered flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-                    <input type="text" className="grow placeholder-neutral" placeholder="Email" />
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
-                    <input type="password" className="grow placeholder-neutral" placeholder="Password" />
-                </label>
-                <button className='btn btn-primary'><Link to={"/"}>Login</Link></button>
-                <p>Don't have an account? <Link to={"/register"} className="link">Register</Link>.</p>
-            </form>
+            <div className="grad"></div>
+            <div id='bg-pattern' className='w-svw h-svh flex justify-center align-middle'>
+                <form className="flex justify-center items-center flex-col w-2/5 gap-4 bg-base-100 z-10 ">
+                    <h1 className='text-5xl font-bold mb-4 -mt-12'>Login</h1>
+                    <label className="input input-bordered flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+                        <input type="text" className="grow placeholder-neutral" placeholder="Email" />
+                    </label>
+                    <label className="input input-bordered flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+                        <input type="password" className="grow placeholder-neutral" placeholder="Password" />
+                    </label>
+                    <button className='btn btn-primary'><Link to={"/"}>Login</Link></button>
+                    <p>Don't have an account? <Link to={"/register"} className="link">Register</Link>.</p>
+                </form>
+            </div>
         </>
     )
 }
